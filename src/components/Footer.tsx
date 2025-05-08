25aad01967d209d2e953b3d4e65fc6de98e85a9b
한상일 <hansang-il@MacBookAir.localdomain>
2025-05-08 11:55:34 +0900

    [Modify] API 호출 문제 수정?

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
-import { View, Text } from "react-native"
+import {View, Text} from 'react-native';
 
-
-export default function Footer () {
-    return (
-        <View className="w-full bg-yellow-500 my-4">
-            <Text className="text-center text-purple-600">나는 Footer</Text>
-        </View>
-    )
+export default function Footer() {
+  return (
+    <View className="w-full bg-yellow-500 my-4">
+      <Text className="text-center text-purple-600">나는 Footer</Text>
+    </View>
+  );
 }
--- src/screens/Sap.tsx
+++ src/screens/Sap.tsx
@@ -1,11 +1,33 @@
 // 삽질용 스크린
 
-import { View, Text } from "react-native";
+import { useQuery } from '@tanstack/react-query';
+import {View, Text} from 'react-native';
+import axios from 'axios';
 
-export default function Sap(){
-    return (
-        <View className="flex flex-col justify-center items-center bg-amber-400">
-            <Text>삽질용 스크린 입니다.</Text>
-        </View>
-    )
+const fetchData = async () => {
+  try {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users/1', {
+    timeout: 5000
+  });
+  
+  return response.data;
+} catch (e : any) {
+  console.error("❌ Error:", e.message, e.code);
+    throw e;
+}
+};
+
+export default function Sap() {
+  const myCustomQuery = useQuery({
+    queryKey: ['myData'],
+    queryFn: fetchData,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  return (
+    <View className="flex flex-col justify-center items-center bg-amber-400">
+      <Text>삽질용 스크린 입니다.</Text>
+      <Text className='py-2'>텍스트 : {myCustomQuery.data?.name}</Text>
+    </View>
+  );
 }
